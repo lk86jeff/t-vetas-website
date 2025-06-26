be93ac3d525be85f7ee1f3b223508093c5dd816b
lk86jeff <lk86jeff@gmail.com>
2025-06-26 17:22:33 +0800

    實績英文部分

---
+++ src/data/customerAliasesEn.ts
@@ -0,0 +1,7 @@
+export const customerAliasesEn = {
+    "Meta": "Global Leading Social Media Company",
+    "Microsoft": "World-renowned Software & Technology Company",
+    "Google": "World's Largest Search Engine Company",
+    "AWS": "World's Largest E-commerce Company",
+    // Add more mappings as needed
+};
